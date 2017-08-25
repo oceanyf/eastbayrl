3e550203411f8031cac8de827fbb8136aba1f28b
rogerstager <roger@stager.us>
2017-08-25 15:00:08 -0700

    Initial checkin

---
+++ hellowoRLd.py
@@ -0,0 +1,14 @@
+# Just test environment runs uring randomly sampled actions
+#
+import gym
+env = gym.make('CartPole-v0')
+for i_episode in range(200):
+    observation = env.reset()
+    for t in range(1000):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
