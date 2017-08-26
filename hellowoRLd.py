5ff2c0834c976a2028d52a56e60f106a7b8745d6
rogerstager <roger@stager.us>
2017-08-25 19:12:08 -0700

    Added NServoArm

--- hellowoRLd.py
+++ hellowoRLd.py
@@ -1,14 +1,20 @@
 # Just test environment runs uring randomly sampled actions
 #
 import gym
-env = gym.make('CartPole-v0')
+import nservoarm
+gym.envs.register(
+    id='NServoArm-v0',
+    entry_point='nservoarm:NServoArmEnv',
+    max_episode_steps=200,
+)
+env = gym.make('NServoArm-v0')
 for i_episode in range(200):
     observation = env.reset()
     for t in range(1000):
         env.render()
-        print(observation)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
+        print(reward,observation)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
