433e45c01e5e556ec8184f9d725d14c0128b2905
rstager <roger@stager.us>
2017-08-26 20:05:47 -0700

    Still under development, but at least it runs. Not sure if the actor policy update is correct

--- ddpg1.py
+++ ddpg1.py
@@ -2,63 +2,104 @@
 #
 import gym
 import keras
+from keras.layers import Dense,Input,Concatenate
+from keras.models import Model
+import keras.backend as K
 import numpy as np
 
-Rsz=128 #replay buffer size
-N=32
-tau=0.9
-gamma=0.9
 
-critic=keras.Sequential()
-actor=keras.Sequential()
-criticp=critic.copy()
-actorp=actor.copy()
+Rsz=1000 #replay buffer size
+N=32 #mini batch size
+tau=0.001
+gamma=0.99
+std=0.01
 
+#start the environment
 env = gym.make('Pendulum-v0')
+print(env.observation_space)
+print(env.action_space)
+
+#critic
+oin = Input(shape=env.observation_space.shape,name='observeration')
+ain = Input(shape=env.action_space.shape,name='action')
+c1=Dense(10,activation='linear')(oin)
+c2=Dense(10,activation='linear')(ain)
+c3=keras.layers.concatenate([c1,c2])
+c3=Dense(10)(c3)
+c3=Dense(1,activation='linear',name='Q')(c3)
+critic=Model([oin,ain],c3)
+critic.summary()
+critic.compile(optimizer='adam',loss='mse')
+
+#actor
+actor=keras.models.Sequential()
+actor.add(Dense(10,input_shape=env.observation_space.shape))
+actor.add(Dense(1,activation='linear'))
+actor.summary()
+actor.compile(optimizer='adam',loss='mse')
+
+#target networks
+criticp=keras.models.clone_model(critic)
+criticp.compile(optimizer='adam',loss='mse')
+actorp=keras.models.clone_model(actor)
+actorp.compile(optimizer='adam',loss='mse')
+
+
+
 
 #replay buffers
 Robs=np.zeros([Rsz] + list(env.observation_space.shape))
 Robs1=np.zeros_like(Robs)
 Rreward=np.zeros((Rsz,))
 Raction=np.zeros([Rsz] + list(env.action_space.shape))
+Rdone=np.zeros((Rsz,))
 
 def exploration(action):
-    return np.zeros_like(action)
+    return np.random.normal(0.0,std,action.shape)
 
 ridx=0
 Rfull=False
 
-for i_episode in range(200):
-    observation = env.reset()
+for i_episode in range(2000):
+    observation1 = env.reset()
+    totalReward=0
     for t in range(1000):
-        action = actor.predict(observation)
+        observation=observation1
+        action = actor.predict(np.expand_dims(observation,axis=0))
         action += exploration(action)
-        observation1, reward, done, info = env.step(action)
-        Robs[ridx], Raction[ridx], Rreward[ridx], Robs1[ridx]=(observation, action, reward, observation1)
-        env.render()
-
+        observation1, reward, done, _ = env.step(action)
+        observation1=observation1[:,0]
+        Robs[ridx]=observation
+        Raction[ridx]=action
+        Rreward[ridx]=reward
+        Rdone[ridx]=done
+        Robs1[ridx]=observation1
         ridx= (ridx + 1) % Rsz
 
-        if Rfull:
+        if Rfull and ridx%10 == 0:
             sample=np.random.choice(Rsz, N)
-            y=Rreward[sample]+gamma*(criticp.predict([Robs[sample],actorp.predict(Robs1[sample])]))
 
-            #update the critic
-            critic.fit([Robs[sample],Raction[sample]],y,batch_size=N, epochs=1, verbose=1)
+            #update critic
+            yq=Rreward[sample]+gamma*(criticp.predict([Robs1[sample],actorp.predict(Robs1[sample])])[:,0])
+            critic.fit([Robs[sample],Raction[sample]],yq,batch_size=N, epochs=1,verbose=0)
 
-            #update the actor
-            #critic.grad()*actor.grad()
+            #update the actor : critic.grad()*actor.grad()
+            cgrad = K.gradients(critic.outputs, critic.inputs[1]) # grad of Q wrt actions
+            f=K.function(critic.inputs,cgrad)
+            actions=actor.predict(Robs[sample])
+            grads=f([Robs[sample],actions])[0]
+            ya=actions+0.1*grads  # nudge action in direction that improves Q
+            actor.fit(Robs[sample], ya, verbose=0, epochs=1)
+            #actor.fit(Robs[sample],grads,verbose=0,epochs=1) # I think this is what others have done.
 
             #update target networks
-            for layerp,layer in zip(criticp.layers,critic.layers):
-                layerp.set_weights(tau*layer.get_weights()+(1-tau)*layerp.get_weights())
-            for layerp, layer in zip(actorp.layers, actor.layers):
-                layerp.set_weights(tau * layer.get_weights() + (1 - tau) * layerp.get_weights())
-
+            criticp.set_weights([tau*w+(1-tau)*wp for wp,w in zip(criticp.get_weights(),critic.get_weights())])
+            actorp.set_weights([tau*w+(1-tau)*wp for wp,w in zip(actorp.get_weights(),actor.get_weights())])
         elif (ridx==0):
             Rfull=True
 
+        env.render()
+        totalReward+=reward
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print("Episode finished after {} timesteps total reward={}".format(t + 1, totalReward))
             break
-        observation=observation1
