143a723482479b8cc91c704c9bc2372590d646c1
rstager <roger@stager.us>
2017-08-30 09:09:24 -0700

    increased model complexity

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import keras
 from keras.layers import Dense,Input,BatchNormalization
 from keras.models import Model
 from keras.optimizers import Adam
-from util import DDPGof,OrnsteinUhlenbeckProcess
+from util import DDPGof,OrnsteinUhlenbeckProcess,ddpgloss
 import nservoarm
 
 
@@ -16,6 +16,9 @@ ain = Input(shape=env.action_space.shape,name='action')
 #c2=Dense(2,activation='linear')(ain)
 c3=keras.layers.concatenate([oin,ain])
 #c3=BatchNormalization()(c3)
+c3=Dense(64,activation='relu')(c3)
+c3=Dense(64,activation='relu')(c3)
+c3=Dense(64,activation='relu')(c3)
 c3=Dense(32,activation='relu')(c3)
 c3=Dense(32,activation='relu')(c3)
 c3=Dense(32,activation='relu')(c3)
@@ -26,10 +29,13 @@ critic.compile(optimizer=Adam(lr=0.001),loss='mse')
 #actor
 actor=keras.models.Sequential()
 actor.add(Dense(10,input_shape=env.observation_space.shape))
+actor.add(Dense(32,activation='relu'))
+actor.add(Dense(32,activation='relu'))
+actor.add(Dense(32,activation='relu'))
 actor.add(Dense(16,activation='relu'))
 actor.add(Dense(16,activation='relu'))
 actor.add(Dense(16,activation='relu'))
 actor.add(Dense(env.action_space.shape[0],activation='linear'))
-actor.compile(optimizer=DDPGof(Adam)(critic, actor, lr=0.001), loss='mse')
+actor.compile(optimizer=DDPGof(Adam)(critic, actor, lr=0.00001), loss=ddpgloss)
 
-exploration=OrnsteinUhlenbeckProcess(size=env.action_space.shape, sigma=2.14, theta=.15, mu=0.,sigma_min=0.3)
+exploration=OrnsteinUhlenbeckProcess(size=env.action_space.shape, sigma=.03, theta=.15, mu=0.,sigma_min=0.01)
