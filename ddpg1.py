933159cda628b7c0daa0095594cf15599c6a65d0
rstager <roger@stager.us>
2017-08-30 09:09:04 -0700

    Added loss ratio tracking

--- ddpg1.py
+++ ddpg1.py
@@ -11,7 +11,7 @@ N=32 # sample size
 tau=0.01
 gamma=0.95
 Oscale=np.array([1,1,.2])
-warmup=25
+warmup=10
 
 
 #import project specifics, such as actor/critic models
@@ -45,7 +45,7 @@ Rdone=np.zeros((Rsz,))
 #set up the plotting
 plt.ion()
 plt.figure(1)
-renderFlag=False
+renderFlag=True
 noiseFlag=True
 def ontype(event): # r  will toggle the rendering, n will togggle noise
     global renderFlag,noiseFlag
@@ -59,19 +59,20 @@ rcnt=0
 Rfull=False
 RewardsHistory = []
 QAccHistory = []
+lossratio=[]
 for i_episode in range(200000):
     observation1 = env.reset()
     RewardsHistory.append(0)
     episode=[]
     for t in range(1000):
         observation=observation1
-
+        #print(observation)
         #take step using the action based on actor
         action = actor.predict(np.expand_dims(observation,axis=0))[0]
         if noiseFlag: action += exploration.sample()
         observation1, reward, done, _ = env.step(action)
         #observation1=observation1[:,0]*Oscale
-        print("obs {} action {} reward {}".format(observation1,action,reward))
+        #print("obs {} action {} reward {}".format(observation1,action,reward))
 
         # insert into replay buffer
         ridx=rcnt%Rsz
@@ -91,6 +92,25 @@ for i_episode in range(200000):
     if (rcnt > N * 5):
         Rfull = True
 
+    # test the accuracy of critic.  Disable actor update if critic is not accurate enough
+
+    discounted_future_reward=Rreward[episode].copy()
+    for i in reversed(range(len(discounted_future_reward)-1)):
+        discounted_future_reward[i]+= gamma * discounted_future_reward[i + 1]
+    shortepisode = episode[:int(len(episode) / 2)]
+    q=critic.predict([Robs[shortepisode], Raction[shortepisode]])[:,0]
+    lossr=np.mean(np.abs(q - discounted_future_reward[:len(q)]))/np.mean(np.abs(discounted_future_reward[:len(q)])+np.abs(q))
+    print("qshape {}".format(q.shape))
+    print("q {}".format(q[:10]))
+    print("R {}".format(discounted_future_reward[:10]))
+    print("diff {}".format(q[:10]-discounted_future_reward[:10]))
+    print("abs {}".format(np.abs(q[:10]-discounted_future_reward[:10])))
+    print("mean abs {}".format(np.mean(np.abs(q[:10]-discounted_future_reward[:10]))))
+    print("mean abs R{}".format(np.mean(np.abs(discounted_future_reward[:10]))))
+    lossratio.append(lossr)
+    trainable=lossratio[-1]<0.1
+
+    print("loss ratio {}".format(lossratio[-1]))
     if Rfull:
         for train_iter in range(len(episode)):
             sample = np.random.choice(min(rcnt, Rsz), N)
@@ -100,8 +120,8 @@ for i_episode in range(200000):
             critic.train_on_batch([Robs[sample], Raction[sample]], yq)
 
             # train the actor to maximize Q
-            if i_episode > warmup:
-                actor.train_on_batch(Robs[sample], np.zeros([N,actor.input_shape[1]]))
+            if i_episode > warmup and trainable:
+                actor.train_on_batch(Robs[sample], np.zeros([N,actor.output_shape[1]]))
 
             # update target networks
             criticp.set_weights([tau * w + (1 - tau) * wp for wp, w in zip(criticp.get_weights(), critic.get_weights())])
@@ -109,11 +129,13 @@ for i_episode in range(200000):
 
     if len(episode)>2:
         print("episod length {}".format(len(episode)))
-        sp=(6,1)
+        sp=(7,1)
         plt.clf()
         plt.subplot(*sp,1)
         #plt.gca().set_ylim([-1.2,1.2])
-        plt.title("Episode {} {}{}".format(i_episode,"Warming" if (i_episode<warmup) else "","/W noise" if noiseFlag else ""))
+        plt.title("Episode {} {}{} {}".format(i_episode,"Warming" if (i_episode<warmup) else ""
+                                           ,"/W noise" if noiseFlag else ""
+                                           ,", training actor" if trainable else ", train critic @ {:.2f}".format(lossratio[-1])))
         for i in range(Robs[episode].shape[1]):
             plt.plot(Robs[episode, i], label='obs {}'.format(i))
         plt.legend(loc=1)
@@ -130,15 +152,13 @@ for i_episode in range(200000):
         plt.legend(loc=1)
         plt.subplot(*sp,4)
         #plt.gca().set_ylim([-15/(1-gamma),50])
-        q = critic.predict([Robs[episode], Raction[episode]])
+        q = critic.predict([Robs[episode], Raction[episode]])[:,0]
         plt.plot(q, 'k', label='Q')
-        qp = criticp.predict([Robs[episode], Raction[episode]])
+        qp = criticp.predict([Robs[episode], Raction[episode]])[:,0]
         plt.plot(qp, 'gray', label='Qp')
-
-        discounted_future_reward=Rreward[episode].copy()
-        for i in reversed(range(len(discounted_future_reward)-1)):
-            discounted_future_reward[i]+= gamma * discounted_future_reward[i + 1]
-        QAccHistory.append(np.mean(np.abs(discounted_future_reward-qp)))
+        QAcc=critic.evaluate([Robs[episode],Raction[episode]],discounted_future_reward)
+        QAccHistory.append(QAcc)
+        #QAccHistory.append(np.mean(np.abs(discounted_future_reward-qp)))
         plt.plot(discounted_future_reward, 'r', label='R')
         plt.legend(loc=1)
         plt.subplot(*sp,5)
@@ -147,6 +167,9 @@ for i_episode in range(200000):
         plt.legend(loc=2)
         plt.subplot(*sp,6)
         plt.plot(QAccHistory, 'r', label='Q error history')
+        plt.legend(loc=2)
+        plt.subplot(*sp,7)
+        plt.plot(lossratio, 'r', label='Q loss ratio history')
         plt.legend(loc=2)
         plt.pause(0.1)
 
