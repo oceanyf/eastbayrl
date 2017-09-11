886dbebddd3362657a07c2365a5f931f7e4f331c
rstager <roger@stager.us>
2017-09-11 11:27:11 -0700

    enabled batchnorm

--- project.py
+++ project.py
@@ -1,12 +1,10 @@
 import gym
 from gym.spaces import Tuple
 import keras
-from keras.layers import Dense,Input,BatchNormalization,Dropout,Conv2D,MaxPool2D,Flatten,Lambda,MaxPooling2D
+from keras.layers import Dense,Input,BatchNormalization,Dropout,Conv2D,AvgPool2D,Flatten,Lambda
 from keras.models import Model
-from keras.optimizers import Adam
-import keras.backend as K
-from util import DDPGof,ornstein_exploration
 import numpy as np
+from keras_util import softmax2d,softargmax,DDPGof,ornstein_exploration
 
 
 datadir="../data"
@@ -29,31 +27,6 @@ def make_arm():
 
 env = make_arm()
 
-# todo: implement 2d softmax
-def softmax2d(x):
-    x=K.reshape(x,())
-    return x
-# e = K.exp(x - K.max(x, axis=axis, keepdims=True))
-# s = K.sum(e, axis=axis, keepdims=True)
-# return e / s
-
-# returns x,y coordinates[0-1) of maximum value for each channel
-def expected_pos(x):
-
-    s1 = K.sum(K.abs(x),axis=-2)
-    s2 = K.sum(K.abs(s1),axis=-2)
-    s2 = 1.0/(s2+.001)
-    xc = K.variable(np.arange(int(x.shape[1]))/(int(x.shape[1])-1))
-    x1 = K.variable(np.ones([x.shape[1]]))
-    yc = K.variable(np.arange(int(x.shape[2]))/(int(x.shape[2])-1))
-    y1 = K.variable(np.ones([x.shape[2]]))
-    xx = K.dot(yc, x)
-    xx = K.dot(x1, xx)
-    xy = K.dot(y1, x)
-    xy = K.dot(xc, xy)
-    nc=K.stack([xx,xy],axis=-1)
-    nc=K.transpose(K.transpose(nc)*K.transpose(s2))
-    return nc
 
 #create actor,critic
 def make_models(everything=False):
@@ -73,22 +46,23 @@ def make_models(everything=False):
         x=Conv2D(16,(3,3),**commonkwargs)(x)
         x=Conv2D(16,(3,3),**commonkwargs)(x)
         #x=Dropout(.25)(x)
-        x=Conv2D(16,(3,3),**commonkwargs)(x)
+        #x=Conv2D(16,(3,3),**commonkwargs)(x)
+        #x=Conv2D(16,(3,3),**commonkwargs)(x)
+        #x=Conv2D(16,(3,3),**commonkwargs)(x)
+        #x=Dropout(.25)(x)
+        #x=Conv2D(16,(3,3),**commonkwargs)(x)
         x=Conv2D(16,(3,3),**commonkwargs)(x)
         x=Conv2D(16,(3,3),**commonkwargs)(x)
         #x=Dropout(.25)(x)
+        x=BatchNormalization()(x)
         x=Conv2D(16,(3,3),**commonkwargs)(x)
-        x=Conv2D(16,(3,3),**commonkwargs)(x)
-        x=Conv2D(16,(3,3),**commonkwargs)(x)
-        #x=Dropout(.25)(x)
-        x=Conv2D(16,(3,3),**commonkwargs)(x)
-        feature_layer=Conv2D(16,(3,3),activation='relu',kernel_regularizer="l2",name='image_softmax')
-        feature_map=feature_layer(x)
-        image_feature_model=Model(imgin,feature_map,name="Image_feature_extraction_model")
+        #x=Conv2D(16,(3,3),activation='softmax',kernel_regularizer="l2",name='image_softmax')
+        x=Lambda(softmax2d,name='image_softmax2d')(x)
+        image_feature_model=Model(imgin,x,name="Image_feature_extraction_model")
 
         # feature estimated coordinate model
         x=image_feature_model(iin)
-        x = Lambda(expected_pos,name="expected_feature_location")(x)
+        x = Lambda(softargmax, name="expected_feature_location")(x)
         flat=Flatten(name='flattend_feature')(x)
         x = Dense(2, activation='linear',kernel_regularizer="l2")(flat)
         locatormodel=Model(oin,x)
@@ -101,7 +75,7 @@ def make_models(everything=False):
         tmp=image_feature_model.get_output_shape_at(1)
         poolsz=np.floor(np.array((tmp[-3],tmp[-2]))/(nzones)) #size of grid
         stridesz=poolsz*(1-overlap)
-        coarse_map=MaxPooling2D(pool_size=poolsz,padding='same',name="coarse_map")(x)
+        coarse_map=AvgPool2D(pool_size=poolsz,padding='same',name="coarse_map")(x)
         coarse_locator=Model(oin,coarse_map)
         #locatormodel.compile(optimizer=Adam(lr=0.001), loss='mse')
 
