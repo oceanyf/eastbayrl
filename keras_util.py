42a0611ad44eda4dfe4fb8f4a7cf37901039cbe2
rstager <roger@stager.us>
2017-09-13 15:59:43 -0700

    Added LrReducer from internet...not tested, but want to hang on to the code.

--- keras_util.py
+++ keras_util.py
@@ -1,3 +1,5 @@
+from keras.callbacks import Callback
+
 import keras.backend as K
 import numpy as np
 
@@ -108,3 +110,33 @@ def ornstein_exploration(space,theta,nfrac=0.01,**kwrds):
     return OrnsteinUhlenbeckProcess(theta,size=space.shape,
                                  sigma=nfrac*np.max(space.high),sigma_min=nfrac*np.min(space.low),**kwrds)
 
+# borrowed from jiumem https://github.com/fchollet/keras/issues/898
+class LrReducer(Callback):
+    def __init__(self, patience=0, reduce_rate=0.5, reduce_nb=10, verbose=1):
+        super(Callback, self).__init__()
+        self.patience = patience
+        self.wait = 0
+        self.best_score = -1.
+        self.reduce_rate = reduce_rate
+        self.current_reduce_nb = 0
+        self.reduce_nb = reduce_nb
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs={}):
+        current_score = logs.get('val_acc')
+        if current_score > self.best_score:
+            self.best_score = current_score
+            self.wait = 0
+            if self.verbose > 0:
+                print('---current best val accuracy: %.3f' % current_score)
+        else:
+            if self.wait >= self.patience:
+                self.current_reduce_nb += 1
+                if self.current_reduce_nb <= 10:
+                    lr = self.model.optimizer.lr.get_value()
+                    self.model.optimizer.lr.set_value(lr*self.reduce_rate)
+                else:
+                    if self.verbose > 0:
+                        print("Epoch %d: early stopping" % (epoch))
+                    self.model.stop_training = True
+            self.wait += 1
